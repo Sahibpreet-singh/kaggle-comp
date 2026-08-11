fcc939c61473bfb1aca2021267586f46b789e77f
Sahibpreet-singh <165685049+Sahibpreet-singh@users.noreply.github.com>
2026-08-11 23:18:48 +0530

    changes

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-class employee:
-   def __init__(self,employee_id,emp_name):
-      self.emp_name=emp_name
-      self.employee_id=employee_id
-
-   def display(self):
-      print(self.emp_name)
-
-class devel(employee):
-   def __init__(self, name, language):
-        super().__init__(name)
-        self.language = language
-   def work(self):
-        print("Writing", self.language)
-
-
-d = devel("Sahib", "Python")
-d.display()
+dsjai
